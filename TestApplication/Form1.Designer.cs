61048dc1a5088887f8dc4c4106272d215f555717
Shiplayer <lllytep199515@yandex.ru>
2017-07-16 23:33:12 +0300

    v1.0.0.0

---
+++ TestApplication/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TestApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addDirWithFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addWavfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.aboutAProgrammToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(260, 172);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setDirectoryToolStripMenuItem,
+            this.addDirWithFilesToolStripMenuItem,
+            this.addWavfileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // setDirectoryToolStripMenuItem
+            // 
+            this.setDirectoryToolStripMenuItem.Name = "setDirectoryToolStripMenuItem";
+            this.setDirectoryToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.setDirectoryToolStripMenuItem.Text = "Set directory";
+            this.setDirectoryToolStripMenuItem.Click += new System.EventHandler(this.setDirectoryToolStripMenuItem_Click);
+            // 
+            // addDirWithFilesToolStripMenuItem
+            // 
+            this.addDirWithFilesToolStripMenuItem.Name = "addDirWithFilesToolStripMenuItem";
+            this.addDirWithFilesToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.addDirWithFilesToolStripMenuItem.Text = "add dir with files";
+            this.addDirWithFilesToolStripMenuItem.Click += new System.EventHandler(this.addDirWithFilesToolStripMenuItem_Click);
+            // 
+            // addWavfileToolStripMenuItem
+            // 
+            this.addWavfileToolStripMenuItem.Name = "addWavfileToolStripMenuItem";
+            this.addWavfileToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.addWavfileToolStripMenuItem.Text = "add wav-file";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutAProgrammToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip2.TabIndex = 3;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 339);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // aboutAProgrammToolStripMenuItem
+            // 
+            this.aboutAProgrammToolStripMenuItem.Name = "aboutAProgrammToolStripMenuItem";
+            this.aboutAProgrammToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.aboutAProgrammToolStripMenuItem.Text = "About a programm";
+            this.aboutAProgrammToolStripMenuItem.Click += new System.EventHandler(this.aboutAProgrammToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 374);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setDirectoryToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem addDirWithFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addWavfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutAProgrammToolStripMenuItem;
+    }
+}
+
---
+++ TestApplication/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApplication
+{
+    public partial class Form1 : Form
+    {
+        private String pathDirWithMedialFiles;
+        private List<string> files;
+        private List<int> associateArray;
+        private SoundPlayer player;
+        private string currentPath;
+
+        public Form1()
+        {
+            currentPath = Directory.GetCurrentDirectory();
+            InitializeComponent();
+            player = new SoundPlayer();
+            pathDirWithMedialFiles = currentPath;
+            label1.Text = pathDirWithMedialFiles;
+            associateArray = new List<int>();
+            files = new List<string>();
+            KeyValuePair<string, string>[] pair = new KeyValuePair<string, string>[10];
+            
+        }
+
+        //custom methods
+        private void DataClear()
+        {
+            listView1.Clear();
+            associateArray.Clear();
+            files.Clear();
+        }
+
+        private void setAllFilesInListView()
+        {
+            ListView.ListViewItemCollection list = listView1.Items;
+            string[] files = Directory.GetFiles(pathDirWithMedialFiles, "*.wav");
+            foreach (string file in files)
+            {
+                associateArray.Add(associateArray.Count + 1);
+                list.Add(file);
+            }
+            Sorting();
+        }
+
+        private void Sorting()
+        {
+            ListView.ListViewItemCollection list = listView1.Items;
+            Random rnd = new Random();
+            int[] rndIndex = new int[list.Count];
+            bool[] checkArr = new bool[list.Count];
+            string[] items = new string[list.Count];
+            for (int index = 0; index < rndIndex.Length; index++)
+            {
+
+                int n = rnd.Next(list.Count);
+                rndIndex[index] = n;
+                if (!checkArr[n])
+                {
+                    checkArr[n] = true;
+                    items[n] = list[index].Text;
+                    if(list[index].BackColor == Color.Red)
+                    {
+                        list[index].BackColor = Color.White;
+                    }
+                }
+                else
+                    index--;
+            }
+            label2.Text = string.Join(", ", rndIndex);
+            string line = "";
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i].Text = items[i];
+            }
+        }
+
+        //Forms methods
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(new FileStream(currentPath + "\\save.data", FileMode.Create));
+            sw.WriteLine(pathDirWithMedialFiles);
+            sw.Close();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (File.Exists(currentPath + "\\save.data"))
+            {
+                StreamReader sr = new StreamReader(currentPath + "\\save.data");
+                pathDirWithMedialFiles = sr.ReadLine();
+                setAllFilesInListView();
+                sr.Close();
+            }
+        }
+
+        //listView1 methods
+        private void listView1_CollectionChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //StripMenu methods
+        private void setDirectoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataClear();
+            FolderBrowserDialog bd = new FolderBrowserDialog();
+            bd.ShowDialog();
+            if (bd.SelectedPath.Length > 0)
+            {
+                pathDirWithMedialFiles = bd.SelectedPath;
+                label1.Text = pathDirWithMedialFiles;
+                setAllFilesInListView();
+            }
+        }
+
+        private void addDirWithFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog bd = new FolderBrowserDialog();
+            bd.ShowDialog();
+            if (bd.SelectedPath.Length > 0)
+            {
+                pathDirWithMedialFiles = bd.SelectedPath;
+                label1.Text = pathDirWithMedialFiles;
+                setAllFilesInListView();
+            }
+        }
+
+        //buttons methods
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection collection = listView1.SelectedItems;
+            if (collection.Count > 0)
+            {
+                collection[0].BackColor = Color.Red;
+                player = new SoundPlayer(collection[0].Text);
+                player.PlaySync();
+            }
+            else
+            {
+                MessageBox.Show(this, "no selected element", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sorting();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (player.IsLoadCompleted)
+            {
+                player.Stop();
+            }
+        }
+
+        private void aboutAProgrammToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutbox = new AboutBox1();
+            aboutbox.Show();
+        }
+    }
+}
